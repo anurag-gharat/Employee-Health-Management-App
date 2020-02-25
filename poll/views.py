47b62150c4c5fe0811c3bb47d6bb598d0419e65d
anurag gharat <anuraggharat55@gmail.com>
2020-02-25 22:05:09 +0530

    "errors sorted"

--- poll/views.py
+++ poll/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect,HttpResponse
+from django.shortcuts import get_object_or_404, render
 from .models import Question,Choice
+from django.urls import reverse
+from django.template import loader
+
 def index(request):
         latest_question_list = Question.objects.order_by('-published_date')
         context = {'latest_question_list':latest_question_list}
@@ -14,3 +19,16 @@ def detail(request,question_id):
 def results(request,question_id):
         question = get_object_or_404(Question,pk=question_id)
         return render(request,'poll/results.html',{'question':question})
+def vote(request,question_id):
+        question = get_object_or_404(Question,pk=question_id)
+        try:
+                selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except [KeyError , Choice.DoesNotExist]:
+                return render(request,'poll/detail.html',{
+                        'question':question,
+                        'error_message':"Select a choice you fucking asshole",
+                })       
+        else:
+                selected_choice.votes +=1
+                selected_choice.save() 
+                return HttpResponseRedirect(reverse('poll:results', args=(question_id,)))

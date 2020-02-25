0611e012904b9488377e55fc7c8d13cb0a29b771
anurag gharat <anuraggharat55@gmail.com>
2020-02-25 20:46:55 +0530

    "basic routing and results page setup"

--- poll/views.py
+++ poll/views.py
@@ -4,3 +4,13 @@ def index(request):
         latest_question_list = Question.objects.order_by('-published_date')
         context = {'latest_question_list':latest_question_list}
         return render(request, 'poll/index.html',context)
+def detail(request,question_id):
+        try:
+                question = Question.objects.get(pk=question_id)
+        except Question.DoesNotExist:
+                raise Http404("Questions Does Not Exist")     
+        return render(request , 'poll/detail.html',{'question':question})   
+
+def results(request,question_id):
+        question = get_object_or_404(Question,pk=question_id)
+        return render(request,'poll/results.html',{'question':question})
